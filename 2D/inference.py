eaad46c082bda052efb26d71d147735fb626d533
Yanke Wang <yankee.wann@gmail.com>
2024-05-28 17:15:33 +0800

    training

--- 2D/inference.py
+++ 2D/inference.py
@@ -2,12 +2,14 @@ import torch
 
 # Model 
 #model = torch.hub.load("/home/2D/weights", "last.pt")  # or yolov5n - yolov5x6, custom
-model = torch.hub.load('yolov5', 'custom', path='last.pt', source='local')
+model = torch.hub.load('yolov5', 'custom', path='/home/Tower_crane_perception/2D/runs/train/exp6/weights/best.pt', source='local')
 # Images
-img = "/home/2D/site_data_test3/test/images/camera1_250_png.rf.d07a9b287cff3a566148a274e34e051f.jpg"  # or file, Path, PIL, OpenCV, numpy, list
+img = "/home/2D/site_data_test3/train/images/camera1_300_png.rf.fff62f6ad0615431f9ffe2edfab52665.jpg"  # or file, Path, PIL, OpenCV, numpy, list
 
+model.iou = 0.01
+model.conf = 0.01
 # Inference
-results = model(img)
-
+results = model(img, size=1280)
+print(results)
 # Results
 results.save()  # or .show(), .save(), .crop(), .pandas(), etc.
--- 2D/utils/dataloaders.py
+++ 2D/utils/dataloaders.py
@@ -914,18 +914,18 @@ class LoadImagesAndLabels(Dataset):
         # img4, labels4 = replicate(img4, labels4)  # replicate
 
         # Augment
-        img4, labels4, segments4 = copy_paste(img4, labels4, segments4, p=self.hyp["copy_paste"])
-        img4, labels4 = random_perspective(
-            img4,
-            labels4,
-            segments4,
-            degrees=self.hyp["degrees"],
-            translate=self.hyp["translate"],
-            scale=self.hyp["scale"],
-            shear=self.hyp["shear"],
-            perspective=self.hyp["perspective"],
-            border=self.mosaic_border,
-        )  # border to remove
+        # img4, labels4, segments4 = copy_paste(img4, labels4, segments4, p=self.hyp["copy_paste"])
+        # img4, labels4 = random_perspective(
+        #     img4,
+        #     labels4,
+        #     segments4,
+        #     degrees=self.hyp["degrees"],
+        #     translate=self.hyp["translate"],
+        #     scale=self.hyp["scale"],
+        #     shear=self.hyp["shear"],
+        #     perspective=self.hyp["perspective"],
+        #     border=self.mosaic_border,
+        # )  # border to remove
 
         return img4, labels4
 
