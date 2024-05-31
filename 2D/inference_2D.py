6f40c012ee0bc5bd44682120d9903c62c3d430b6
Yanke Wang <yankee.wann@gmail.com>
2024-05-31 11:16:42 +0800

    pixel2lidar

---
+++ 2D/inference_2D.py
@@ -0,0 +1,148 @@
+import torch
+import cv2
+from pathlib import Path
+from tower_utils import pixel2Camera, camera2Lidar, find_closest_cluster, get_3d_box_from_points
+import open3d as o3d
+import time
+import numpy as np
+
+from tracking import tracking_diff
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent/"data_process"))
+sys.path.append(str(Path(__file__).parent))
+
+from pointCloud_clustering import pcd_clustering
+
+print(sys.path)
+
+ORI_RESO = True
+# Model 
+#model = torch.hub.load("/home/2D/weights", "last.pt")  # or yolov5n - yolov5x6, custom
+model = torch.hub.load('yolov5', 'custom', path='/home/Tower_crane_perception/2D/runs/train/exp2/weights/last.pt', source='local')
+model.iou = 0.2
+model.conf = 0.7
+# Images
+video_path = Path("/home/2D/inference/sync_camera_lidar/camera1/")
+lidar_path = Path("/home/2D/inference/sync_camera_lidar/ouster1/")
+image_list = sorted(video_path.rglob("*.png"), key=lambda a: int(str(a).split("_")[-1].split(".")[0]))
+lidar_list = sorted(lidar_path.rglob("*.pcd"), key=lambda a: int(str(a).split("_")[-1].split(".")[0]))
+save_path = Path("/home/Tower_crane_perception/2D/runs/inference/2d_lidar/")
+save_path.mkdir(exist_ok=True, parents=True)
+
+if ORI_RESO:
+    size = (3840, 2160)
+else:
+    size = (1344, 768)
+out = cv2.VideoWriter(str(save_path / "video_comp.avi"), cv2.VideoWriter_fourcc(*'MPEG'), 10, size, True)
+
+# vis = o3d.visualization.Visualizer()
+# vis.create_window(visible=True) 
+
+
+x_ratio = 3840 / 1344
+y_ratio = 2160 / 768
+last_pred = None
+start_tracking = False
+end_tracking = False
+last_detected = False
+not_detected_cnt = 0
+not_detected_THRE = 20
+
+previsous_pts = []
+threed_boxes = []
+for n, (i_p, l_p) in enumerate(zip(image_list[80:600], lidar_list[80:600])):
+    print(i_p, l_p)
+    img = cv2.imread(str(i_p))
+    img_input = cv2.resize(img, (1344, 768), cv2.INTER_CUBIC)
+    img_input = cv2.cvtColor(img_input, cv2.COLOR_BGR2RGB)
+    
+    # Inference
+    results = model(img_input, size=1344)
+    # Results
+    #results.save(save_dir=save_path)  # or .show(), .save(), .crop(), .pandas(), etc.
+    # images = results.render()
+    # im = cv2.cvtColor(images[0], cv2.COLOR_RGB2BGR)
+    # #cv2.imwrite(str(save_path/(str(n) + '.png')), im)
+    # out.write(im)
+
+    pred = results.pred[0].cpu().numpy()
+
+    if pred.shape[0] > 0:
+        last_pred = pred
+        start_tracking = True
+        last_detected = True
+    else:
+        if start_tracking:
+            pred = last_pred
+        if not last_detected:
+            not_detected_cnt += 1
+        else:
+            not_detected_cnt = 0
+
+        if not_detected_cnt > not_detected_THRE:
+            if start_tracking:
+                end_tracking = True
+        last_detected = False
+    #print(start_tracking, end_tracking)
+    if start_tracking:
+        if not end_tracking:
+            #print(pred)
+            # if len(previsous_pts)>3:
+            #     twod_box = tracking_diff(previsous_pts, 3)
+            # twod_box = []
+            if ORI_RESO:
+                pixel_pt = ((pred[0, 0]+pred[0, 2])*x_ratio/2, (pred[0, 1]+pred[0, 3])*x_ratio/2, 1)
+                print(pixel_pt, pred[0, 4])
+                camera_pt = pixel2Camera(pixel_pt, 50.0)
+                lidar_pt = camera2Lidar(camera_pt)
+                #print(pixel_pt, camera_pt, lidar_pt)
+                pcd, all_cluster_centers, all_cluster_aligments = pcd_clustering(str(l_p))
+
+
+                # #print(all_cluster_centers)
+                # # all_cluster_centers = [[100, 40, 50],
+                # #                        [200, -50, 60],
+                # #                        [-40, 60, 80]]
+                ind = find_closest_cluster(vecs=all_cluster_centers, vec1=lidar_pt[:3])
+                threed_box = get_3d_box_from_points(all_cluster_aligments[all_cluster_aligments[:, 3]==ind])
+                threed_boxes.append(threed_box)
+
+                # vis.add_geometry(pcd)
+                # vis.run()
+                # # vis.update_geometry(pcd)
+                # # vis.poll_events()
+                # # vis.update_renderer()
+                # time.sleep(1)
+                # vis.capture_screen_image(str(save_path / (str(n) + ".png")))
+                # print(all_cluster_centers[ind], threed_box)
+                # tracking_diff()
+                img = cv2.rectangle(img, 
+                                    pt1=(int(pred[0, 0]*x_ratio), int(pred[0, 1]*y_ratio)), 
+                                    pt2=(int(pred[0, 2]*x_ratio), int(pred[0, 3]*y_ratio)), 
+                                    color=(0, 0, 255), 
+                                    thickness=10)
+            else:
+                img = cv2.cvtColor(img_input, cv2.COLOR_RGB2BGR)
+                img = cv2.rectangle(img, 
+                                    pt1=(int(pred[0, 0]), int(pred[0, 1])), 
+                                    pt2=(int(pred[0, 2]), int(pred[0, 3])), 
+                                    color=(0, 0, 255), 
+                                    thickness=5)
+            #previsous_pts.append(twod_box)
+                
+    if not start_tracking or end_tracking:
+        threed_boxes.append([0,0,0,0,0,0])
+        if not ORI_RESO:
+            img = cv2.cvtColor(img_input, cv2.COLOR_RGB2BGR)
+    
+    out.write(img)
+
+    # if n>600:
+    #     break
+# vis.destroy_window()
+cv2.destroyAllWindows()
+out.release()
+np.savetxt(save_path/'threed_boxes.txt', np.array(threed_boxes), fmt='%1.4e')
---
+++ 2D/tower_utils.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+Intrinsic = np.array([[3398.232603, 0, 1895.977318],
+                      [0, 3409.324671, 1081.942343],
+                      [0,0,1]])
+
+c2L = np.array([[-0.00380224, -0.999927, -0.0114549, 0.0468632],
+                [-0.480837, 0.011872, -0.87673, 0.810458],
+                [0.876802, 0.00217442, -0.480847, 0.903057],
+                [0,0,0,1]])
+
+def pixel2Camera(pixel_pt=[], distance = 1.0):
+    """
+    pixel_pt: x,y,1
+    distance: [m]
+    """
+    n_pt = np.dot(np.linalg.inv(Intrinsic), pixel_pt)
+
+    ratio = distance / np.sqrt(n_pt[0]**2 + n_pt[1]**2 + n_pt[2]**2)
+
+    return n_pt*ratio
+
+def camera2Lidar(camera_pt=[]):
+    c_pt = np.ones((4, 1), np.float32)
+    c_pt[:3, 0] = camera_pt
+    return np.dot(c2L, c_pt)
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between_vectors(v1=[], v2=[]):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+def find_closest_cluster(vecs=[[]], vec1=[]):
+    angles = []
+    for i in range(len(vecs)):
+        angle = angle_between_vectors(vecs[i], vec1)
+        angles.append(angle)
+
+    return np.argmin(angles)
+
+def get_3d_box_from_points(pts=[]):
+    x_min = np.min(pts[:, 0])
+    x_max = np.max(pts[:, 0])
+
+    y_min = np.min(pts[:, 1])
+    y_max = np.max(pts[:, 1])
+
+    z_min = np.min(pts[:, 2])
+    z_max = np.max(pts[:, 1])
+
+
+    return (x_min, x_max, y_min, y_max, z_min, z_max)
