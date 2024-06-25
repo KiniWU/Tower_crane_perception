1767f2c403fbdb6b0be615ed9b505ef2f0ceb799
Yanke Wang <yankee.wann@gmail.com>
2024-06-25 20:22:13 +0800

    update ground333_pipeline

--- 333/ground333_utils.py
+++ 333/ground333_utils.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from config import Intrinsic, c2L, camera_dist
 
-
 def check_lift_start(pred_history,frequency=5,interval=5):
     # check whether mic lifting start 
     # inputs :
@@ -58,6 +57,7 @@ def obj_loss_filter(obj_pred_history,pred,object_class):
             obj_loss_flag = False
 
     if obj_loss_flag == True:
+        # print(obj_pred_history.shape)
         pred = np.vstack((pred,obj_pred_history[-1,:]))
 
     return pred
@@ -141,10 +141,44 @@ def ground333_detect_new(model, img, std_img_size=1280):
     pred[:,3] = pred[:,3]*HEIGHT_RATIO
 
     # convert image to original format after yolo reference 
-    img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_CUBIC)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    return pred,img
+    # img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_CUBIC)
+    # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    return pred
 
-def ground333_tracking():
-    pass
-    return
+def pred_filtering(hook_pred_history,mic_pred_history,frame_pred_history,pred):
+
+    pred = obj_multi_filter(hook_pred_history, pred,0)
+    pred = obj_multi_filter(mic_pred_history,  pred,1)
+    pred = obj_multi_filter(frame_pred_history,pred,2)
+    
+    pred = obj_loss_filter(hook_pred_history, pred,0)
+    pred = obj_loss_filter(mic_pred_history,  pred,1)
+    pred = obj_loss_filter(frame_pred_history,pred,2)
+
+    return pred
+
+def check_mic_lifting(hook_pred_history,mic_pred_history,frame_pred_history,pred,is_lift_start):
+    hook_pred_history  = obj_pred_history(hook_pred_history, pred,0) #0-hook;1-mic;2-frame;3-people
+    mic_pred_history   = obj_pred_history(mic_pred_history,  pred,1) #0-hook;1-mic;2-frame;3-people
+    frame_pred_history = obj_pred_history(frame_pred_history,pred,2) #0-hook;1-mic;2-frame;3-people
+
+    is_hook_start     = check_lift_start(hook_pred_history)
+    is_mic_start      = check_lift_start(mic_pred_history)
+    is_frame_start    = check_lift_start(frame_pred_history)
+
+    if (is_hook_start and is_mic_start) or (is_hook_start and is_frame_start) or (is_mic_start and is_frame_start):
+        is_lift_start = True
+        print(is_lift_start)
+
+    return hook_pred_history, mic_pred_history,frame_pred_history, is_lift_start
+
+def plotting(img, pred,is_lift_start):
+    for i in np.arange(pred.shape[0]):
+        colors = [[0,0,0],(0, 255, 0),(0,0,255),(255, 0, 0)]
+        img    = cv2.rectangle(img, 
+                    pt1=(int(pred[i, 0]), int(pred[i, 1])), 
+                    pt2=(int(pred[i, 2]), int(pred[i, 3])), 
+                    color=colors[int(pred[i,5])], 
+                    thickness=10)
+    if is_lift_start == True:
+        cv2.putText(img,'the mic lifting start:',(100,100),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),1)
