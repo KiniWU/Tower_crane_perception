f0b99fa0548c5a2f8a026895e42564b08c3fec81
Yanke Wang <yankee.wann@gmail.com>
2024-07-03 09:23:51 +0800

    train 333_v1_2 model

--- 2D/preprocessing.py
+++ 2D/preprocessing.py
@@ -4,8 +4,16 @@ import cv2
 
 
 
-img = cv2.imread("/home/tower_crane_data/gen_dataset/333-v3/train/images/hik_1_png.rf.8dd31f9e138b2f105b88d3985b53c66e.jpg")
+img = cv2.imread("/home/tower_crane_data/dataset_333/2024-06-14-09-33-24_ruian/pic/hik_1.png")
+print(img)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 h,s,v = cv2.split(img_hsv)
-bright_pixel = np.amax(v)
-print(bright_pixel)
+
+intensity_factor = 1.7
+
+increased_value_channel = np.clip(v * intensity_factor, 0, 255).astype(np.uint8)
+img_hsv[:, :, 2] = increased_value_channel
+
+enhanced_image = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
+
+cv2.imwrite("res/image_enhanced.png", enhanced_image)
